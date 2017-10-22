d58c1326fb075d4d91fa6db1d79c2545b5606697
Austin Hansen <hello@austinhansen.io>
2017-10-22 11:07:16 -0700

    better banner text

--- app/javascript/packs/components/App.js
+++ app/javascript/packs/components/App.js
@@ -5,10 +5,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">React Table!</h1>
-        </header>
-        <UserTable />
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-3">React Table!</h1>
+            <p className="lead">This incredible table was made using <a href="https://reactjs.org/">React</a>, <a href="http://rubyonrails.org/">Rails</a>, <a href="https://getbootstrap.com/docs/3.3/">Bootstrap</a>, and <a href="https://github.com/react-tools/react-table">React Table</a>.</p>
+          </div>
+          <UserTable />
+        </div>
       </div>
     );
   }
