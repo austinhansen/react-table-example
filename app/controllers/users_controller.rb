ff3fd5492719850c546edf73b15db1ab532027a9
Austin Hansen <hello@austinhansen.io>
2017-10-22 09:12:22 -0700

    add users controller and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: { status: 200,
+                   data: @users }
+  end
+
+  def generate_csv
+  end
+end
